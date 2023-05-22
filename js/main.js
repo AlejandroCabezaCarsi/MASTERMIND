a08c4cee2c37ab7b89c72d107e5039e8ef282cb9
Alex <cabezacarsialejandro@gmail.com>
2023-05-22 19:56:43 +0200

    [ADD] Added the color picker function to colorPickerFacil.html, added default colors to the election divs

--- js/main.js
+++ js/main.js
@@ -1,6 +1,36 @@
-let
-let
-let
+let contenedor1 = document.getElementById ('contenedorColorPickerFacil1');
+let contenedor2 = document.getElementById ('contenedorColorPickerFacil2');
+let contenedor3 = document.getElementById ('contenedorColorPickerFacil3');
+let contenedor4 = document.getElementById ('contenedorColorPickerFacil4');
+
+let colorPickerExistente = false; 
+
+let arrayColores = []
+
+const apareceColorPicker = () => { 
+
+    if (colorPickerExistente == false ){
+    
+    const colorPicker = document.getElementById('colorPicker'); 
+    
+    const inputColorPicker = document.createElement('input'); 
+    inputColorPicker.type = 'color'; 
+
+    colorPicker.appendChild(inputColorPicker)
+
+    colorPicker.style.display = 'block'; }
+    
+    colorPickerExistente = true; 
+
+}
+
+contenedor1.style.backgroundColor = "#AC92EC"; 
+contenedor2.style.backgroundColor = "#F6BB42"; 
+contenedor3.style.backgroundColor = "#ED5565"; 
+contenedor4.style.backgroundColor = "#A0D468"; 
+
+
+
 
 
 
